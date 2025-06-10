4c14f9b62c62f45b67484d51300201fb1b267de3
GrimDoom13 <doom7787@gmail.com>
2025-06-09 18:20:03 -0700

    Nothing to see here

--- script.js
+++ script.js
@@ -314,6 +314,7 @@ function updateSlotVisibility() {
         
         updateEquipmentSlots(totalEquipmentSlots);
         updateGrenadeSlots(totalGrenadeSlots);
+        
 }
 
 function updateEquipmentSlots(totalSlots) {
